56e18d3e6fc298894aba2e2c14afd9bfc1805d14
christophehad <50202327+christophehad@users.noreply.github.com>
2021-01-18 14:10:32 +0200

    common api communication

---
+++ backend/api/v1/api_config.js
@@ -0,0 +1,20 @@
+module.exports.DEBUG = false;
+
+// Communication with API
+module.exports.successJSON = function successJSON() {
+    return {
+        success: true,
+        token: ''
+    };
+}
+
+/**
+ * Failure JSON to be returned
+ * @param {String} error 
+ */
+module.exports.failureJSON = function failureJSON(error) {
+    return {
+        success: false,
+        error: error
+    }
+}
