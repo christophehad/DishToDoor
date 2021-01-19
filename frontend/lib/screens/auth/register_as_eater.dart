97670063c430e57fe1c2c2b2a26b89272fe9b0e0
Anthony Kahwaji <aak104@mail.aub.edu>
2021-01-19 19:40:00 +0200

    signup process without API

---
+++ frontend/lib/screens/auth/register_as_eater.dart
@@ -0,0 +1,195 @@
+import 'package:dishtodoor/app_properties.dart';
+import 'package:flutter/material.dart';
+import 'register_as_cook.dart';
+
+class RegisterEaterPage extends StatefulWidget {
+  @override
+  _RegisterEaterPageState createState() => _RegisterEaterPageState();
+}
+
+class _RegisterEaterPageState extends State<RegisterEaterPage> {
+  TextEditingController email =
+      TextEditingController(text: 'example@email.com');
+
+  TextEditingController password = TextEditingController(text: '12345678');
+
+  TextEditingController fname = TextEditingController(text: 'First Name');
+  TextEditingController lname = TextEditingController(text: 'Last Name');
+
+  @override
+  Widget build(BuildContext context) {
+    Widget title = Text(
+      'Welcome!',
+      style: TextStyle(
+          color: Colors.white,
+          fontSize: 34.0,
+          fontWeight: FontWeight.bold,
+          shadows: [
+            BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.15),
+              offset: Offset(0, 5),
+              blurRadius: 10.0,
+            )
+          ]),
+    );
+
+    Widget subTitle = Padding(
+        padding: const EdgeInsets.only(right: 56.0),
+        child: Text(
+          'Sign-up to start your cooking journey',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+          ),
+        ));
+
+    Widget registerButton = Positioned(
+      left: MediaQuery.of(context).size.width / 4,
+      bottom: 0,
+      child: InkWell(
+        //MODIFY to different button - here onTap should communicate with backend
+        onTap: () {},
+        child: Container(
+          width: MediaQuery.of(context).size.width / 2,
+          height: 80,
+          child: Center(
+              child: new Text("Register",
+                  style: const TextStyle(
+                      color: const Color(0xfffefefe),
+                      fontWeight: FontWeight.w600,
+                      fontStyle: FontStyle.normal,
+                      fontSize: 20.0))),
+          decoration: BoxDecoration(
+              gradient: mainButton,
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.16),
+                  offset: Offset(0, 5),
+                  blurRadius: 10.0,
+                )
+              ],
+              borderRadius: BorderRadius.circular(9.0)),
+        ),
+      ),
+    );
+
+    Widget registerForm = Container(
+      height: 320,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            height: 250,
+            width: MediaQuery.of(context).size.width,
+            padding: const EdgeInsets.only(left: 32.0, right: 12.0),
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(255, 255, 255, 0.8),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: TextField(
+                    controller: fname,
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: TextField(
+                    controller: lname,
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: TextField(
+                    controller: email,
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: TextField(
+                    controller: password,
+                    style: TextStyle(fontSize: 16.0),
+                    obscureText: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          registerButton,
+        ],
+      ),
+    );
+
+    Widget socialRegister = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Sign-up as a cook',
+          style: TextStyle(
+              fontSize: 13.0, fontStyle: FontStyle.italic, color: Colors.white),
+        ),
+        IconButton(
+          icon: Icon(Icons.room_service),
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => RegisterCookPage()));
+          },
+          color: Colors.white,
+        ),
+      ],
+    );
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/background_signup_eater.jpg'),
+                    fit: BoxFit.cover)),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: transparentYellow,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 28.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Spacer(flex: 3),
+                title,
+                Spacer(),
+                subTitle,
+                Spacer(flex: 2),
+                registerForm,
+                Spacer(flex: 2),
+                Padding(
+                    padding: EdgeInsets.only(bottom: 20), child: socialRegister)
+              ],
+            ),
+          ),
+          Positioned(
+            top: 35,
+            left: 5,
+            child: IconButton(
+              color: Colors.white,
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
