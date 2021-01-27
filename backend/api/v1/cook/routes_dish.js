fdda047ed5bf20fcd7467f9d51feb3c6ea598e88
christophehad <50202327+christophehad@users.noreply.github.com>
2021-01-27 13:59:54 +0200

    cookdish add + get + upload pics to Azure

--- backend/api/v1/cook/routes_dish.js
+++ backend/api/v1/cook/routes_dish.js
@@ -1,4 +1,4 @@
-const express = require('express'); 
+const express = require('express');
 
 const apiConfig = require('../api_config');
 const DEBUG = apiConfig.DEBUG;
@@ -7,10 +7,24 @@ const genDishAPI = apiConfig.genDish;
 const successJSON = apiConfig.successJSON;
 const failureJSON = apiConfig.failureJSON;
 
+// module for uploading pics
+const multer = require('multer');
+const fs = require('fs');
+const extension = require('mime-types').extension;
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => { cb(null, apiConfig.tmpPath + 'dish_pics/')},
+    filename: (req,file,cb) => {
+        const uniquePref = Date.now() + '-' + Math.round(Math.random()*1E9);
+        cb(null, uniquePref+'.'+extension(file.mimetype));
+    }
+});
+const upload = multer({ storage: storage });
+
 const router = express.Router();
 
 // Generic dishes functions
 const genDish = require('./dish/gen_dish');
+const cookDish = require('./dish/cook_dish');
 
 router.post('/gen-dish/add', (req,res,next) => {
     if (DEBUG) console.log(req.body);
@@ -43,16 +57,67 @@ router.get('/gen-dish/search', (req,res,next) => {
     })
 })
 
-
 // get the gendish categories
 router.get('/gen-dish/categories', (req,res,next) => {
-    if (DEBUG) console.log(req.body);
-
     let genCategories = genDish.getCategories();
     let toSend = successJSON();
     toSend.gen_dish_categories = genCategories;
     res.json(toSend);
 })
 
+
+router.post('/cook-dish/add', upload.single('dish_pic'), (req,res,next) => {
+    if (DEBUG) console.log(req.body);
+
+    let cook_id=req.user,
+        custom_name = req.body.custom_name ? req.body.custom_name : undefined;
+        gendish_id=req.body.gendish_id, price=req.body.price, description=req.body.description, category=req.body.category;
+    
+    let dishpic_path, dishpic_filename;
+    if (req.file) {
+        // contains the dish_pic
+        if (DEBUG) console.log(`Received file: ${req.file.filename}, orig: ${req.file.originalname}, path: ${req.file.path}`);
+        dishpic_path = req.file.path;
+        dishpic_filename = req.file.filename;
+    }
+    else {
+        // without the dish_pic
+        dishpic_path = undefined, dishpic_filename = undefined;
+    }
+    cookDish.add(gendish_id, cook_id, custom_name, price, category, description, dishpic_path, dishpic_filename, (err, cookdish_id, message) => {
+        if (err) return next(err);
+        if (!cookdish_id) return res.json(failureJSON(message));
+        res.json(successJSON());
+    })
+})
+
+// get the cookdishes
+router.get('/cook-dish/get', (req, res, next) => {
+    if (DEBUG) console.log(req.body);
+
+    let cook_id = req.user;
+    if (Object.keys(req.query).length == 0) {
+        // get all the cookdishes
+        cookDish.getAll(cook_id, (err,cookdishes,message) => {
+            if (err) return next(err);
+            if (!cookdishes) return res.json(failureJSON(message));
+
+            let toSend = successJSON();
+            /** @type {apiConfig.CookDishAPI[]} */
+            let cook_dishesAPI = [];
+            for (const cookdish of cookdishes) {
+                cook_dishesAPI.push({
+                    dish_id: cookdish.dish_id, gendish_id: cookdish.gendish_id, name: cookdish.name,
+                    price: cookdish.price, category: cookdish.category, description: cookdish.description, dish_pic: cookdish.dish_pic
+                });
+            }
+            toSend.cook_dishes = cook_dishesAPI;
+            res.json(toSend);
+        })
+    }
+    else {
+        res.sendStatus(400);
+    }
+})
 
 module.exports = router;
--- backend/database/schemes.js
+++ backend/database/schemes.js
@@ -23,3 +23,34 @@ exports.genDish = function (id,name,category) {
         id: id, name: name, category: category
     }
 }
+
+/**
+ * @typedef {Object} CookDish
+ * @property {Number} dish_id
+ * @property {Number} gendish_id
+ * @property {Number} cook_id
+ * @property {String} name
+ * @property {Number} price
+ * @property {String} category
+ * @property {String} label
+ * @property {String} description
+ * @property {String} dish_pic
+ */
+
+ /**
+  * Generate a CookDish
+  * @returns {CookDish}
+  */
+ exports.cookDish = function (dish_id,gendish_id,cook_id,name,price,category,label,description,dish_pic) {
+     return {
+         dish_id: dish_id, gendish_id: gendish_id, cook_id: cook_id, name: name, price: price, category: category,
+         label: label, description: description, dish_pic: dish_pic
+     }
+ }
+
+/**
+* CookDish search callback
+* @callback cookDishSearchCallback
+* @param {String} err
+* @param {CookDish[]} cookdishes
+*/
