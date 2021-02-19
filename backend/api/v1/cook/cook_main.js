4d386f252be4bde00d7a42963b1c591d41a90f1d
christophehad <50202327+christophehad@users.noreply.github.com>
2021-02-19 14:18:34 +0200

    automatic creation of tmp folder

--- backend/api/v1/cook/cook_main.js
+++ backend/api/v1/cook/cook_main.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const passportJWT = require('./auth/jwt');
+const fs = require('fs');
+const apiConfig = require('../api_config');
 
 /**
  * Main Module for cook
  * @param {express.Application} app 
  */
 module.exports = function(app) {
+    // create tmp folder if not present
+
+    if (!fs.existsSync(apiConfig.tmpPath)) {
+        fs.mkdirSync(apiConfig.tmpPath);
+        console.log('Created tmp Path at ' + apiConfig.tmpPath);
+    }
 
     // Load auth routes
     const routesAuth = require('./routes_auth');
