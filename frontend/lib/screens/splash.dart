ca5b4c7b9514f8b8704802965f1a6d48909ed5ac
Anthony Kahwaji <aak104@mail.aub.edu>
2021-02-20 19:39:45 +0200

    streamlined cook location sending +loading screens

--- frontend/lib/screens/splash.dart
+++ frontend/lib/screens/splash.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:dishtodoor/screens/auth/login.dart';
+import 'package:dishtodoor/screens/page_navigator_cook.dart';
 import 'package:dishtodoor/screens/page_navigator_eater.dart';
 import 'package:flutter/material.dart';
 import 'package:dishtodoor/config/config.dart';
@@ -46,35 +47,81 @@ class _SplashScreenState extends State<SplashScreen>
     String resT = await storage.read(key: "token");
     String resE = await storage.read(key: "email");
     String resP = await storage.read(key: "pass");
+    String resType = await storage.read(key: "type");
     print(resE);
     print(resP);
+    print(resType);
 
-    final http.Response response = await http.post(
-        baseURL + '/eater/login-email',
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8'
-        },
-        body: jsonEncode(<String, String>{
-          "email": resE,
-          "password": resP,
-        }));
+    if (resType == "eater") {
+      final http.Response response = await http.post(
+          baseURL + '/eater/login-email',
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8'
+          },
+          body: jsonEncode(<String, String>{
+            "email": resE,
+            "password": resP,
+          }));
 
-    if (response.statusCode == 200) {
-      dynamic decoded = jsonDecode(response.body);
-      print("Received: " + decoded.toString());
-      bool success = decoded['success'];
-      await storage.delete(key: 'token');
-      await storage.write(key: 'token', value: decoded['token']);
-      if (success) {
-        //_registerSuccessfulAlert();
-        Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (_) => PageNavigatorEater()));
-        print("Successful!");
+      if (response.statusCode == 200) {
+        dynamic decoded = jsonDecode(response.body);
+        print("Received: " + decoded.toString());
+        bool success = decoded['success'];
+        await storage.delete(key: 'token');
+        await storage.write(key: 'token', value: decoded['token']);
+        if (success) {
+          //_registerSuccessfulAlert();
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (_) => PageNavigatorEater()));
+          print("Successful!");
+        } else {
+          print("Error: " + decoded['error']);
+        }
       } else {
-        print("Error: " + decoded['error']);
+        print("An unkown error occured");
       }
     } else {
-      print("An unkown error occured");
+      final http.Response response = await http.post(
+          baseURL + '/cook/login-email',
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8'
+          },
+          body: jsonEncode(<String, String>{
+            "email": resE,
+            "password": resP,
+          }));
+
+      if (response.statusCode == 200) {
+        dynamic decoded = jsonDecode(response.body);
+        print("Received: " + decoded.toString());
+        bool success = decoded['success'];
+        if (success) {
+          //_registerSuccessfulAlert();
+          //add cook info
+          //add cook location only if hasn't been previously stored
+          bool locAvailable = await storage.containsKey(key: 'location');
+          if (locAvailable == false) {
+            cookLocation.sendLoc().then((value) async {
+              await storage.write(
+                  key: 'location',
+                  value: (cookLocation.cookLocation.latitude.toString() +
+                      ',' +
+                      cookLocation.cookLocation.latitude.toString()));
+            });
+          }
+          // if(gotLocation == false){
+
+          // }
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (_) => PageNavigatorCook()));
+          print("Successful!");
+          //print("Your token is" + globals.token);
+        } else {
+          print("Error: " + decoded['error']);
+        }
+      } else {
+        print("An unkown error occured");
+      }
     }
   }
 
